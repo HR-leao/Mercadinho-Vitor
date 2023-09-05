73fd7661e8329f35d1f1c7daaa30aa1b8e63a5c5
Hendrio <bigdatahi7@gmail.com>
2023-09-04 21:19:24 -0300

    Up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-
+//COLOCANDO COR NO FUNDO DO BOTÃO DO SLIDE
 document.querySelectorAll('.manual-btn').forEach((e)=>{
     e.addEventListener('click', ()=>{
     // Remover a classe 'active' de todos os elementos com a mesma classe
@@ -10,3 +10,30 @@ document.querySelectorAll('.manual-btn').forEach((e)=>{
     })
 
 })
+//___________________________________________________________________________________________________
+let radios = document.querySelectorAll('.slides .radios')
+let teste = document.querySelector('.slide.first')
+let valor = 0;
+
+
+
+function testeq () {
+    if(valor === 0){
+        setTimeout(function(){
+            teste.style.marginLeft="-25%";
+            valor++;
+            console.log(valor);
+            testeq();
+        },3000)
+    }if(valor===1){
+        setTimeout(function(){
+            teste.style.marginLeft="-50%";
+            valor++
+            console.log(valor);
+            testeq();
+        },3000)
+    }
+}
+
+testeq()
+setInterval(testeq,4000)
