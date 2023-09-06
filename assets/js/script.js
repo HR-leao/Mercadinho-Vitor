e776d611fd1d6cc8e7cf86e87a1952a4a11f4109
Hendrio <bigdatahi7@gmail.com>
2023-09-05 21:24:06 -0300

    Pensei-que-tinha-terminado-kkkkkkk

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,89 @@
 //COLOCANDO COR NO FUNDO DO BOTÃO DO SLIDE
-document.querySelectorAll('.manual-btn').forEach((e)=>{
+let manualBtn = document.querySelectorAll('.manual-btn').forEach((e)=>{
     e.addEventListener('click', ()=>{
     // Remover a classe 'active' de todos os elementos com a mesma classe
         document.querySelectorAll('.manual-btn').forEach((ele)=>{
         ele.classList.remove('active');
+        
         });
     // Adicionar a classe 'active' ao elemento clicado
         e.classList.add('active')
     })
-    
+     
 })
 //___________________________________________________________________________________________________
 
 let count = 1;
-document.querySelector('#radio1').checked = true;
+let radio = document.querySelector('#radio1').checked = true;
+let auto = document.querySelectorAll('.auto')
+let auto1 = document.querySelector('.auto-btn1')
+let auto2 = document.querySelector('.auto-btn2')
+let auto3 = document.querySelector('.auto-btn3')
+let tet = document.querySelectorAll('.manual-btn')
+
+
+
+
+tet.forEach((e)=>{
+    e.addEventListener('click', ()=>{
+        if(tet){
+             auto3.classList.remove('active')
+             auto2.classList.remove('active')
+             auto1.classList.remove('active')
+             setInterval(function(){
+                e.classList.remove('active')
+             },3000)
+        }
+    })
+})
+
+
+
+
+function teste () {
+    
+    setTimeout(function(){
+        if(count === 1){
+        auto1.classList.add('active')
+        auto3.classList.remove('active')
+        console.log(count)
+            
+    }
+    },3000)
+    
+    setInterval(function(){
+        if(count === 2){
+        auto1.classList.remove('active')
+        auto2.classList.add('active')
+        console.log(count)
+    }
+    },3000)
+    setInterval(function(){
+        if(count===3) {
+        auto2.classList.remove('active')
+        auto3.classList.add('active')
+         console.log(count)
+    }
+    },3000)
+}
+
 
 setInterval(function() {
+   
+    teste()
     nextImage()
 }, 3000)
 
 
 function nextImage() {
+
     count++;
     if(count > 3) {
+        
         count =1;
+        
     }
-
     let v = document.querySelector('#radio'+count).checked = true;
-    
+
 }
+
