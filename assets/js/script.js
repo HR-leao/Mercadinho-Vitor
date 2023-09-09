0072620343d0a9b6956ca695e31da196690f3c83
Hendrio <bigdatahi7@gmail.com>
2023-09-09 17:21:28 -0300

    Up

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 $(document).ready(function () {
     $('.slides').slick({
         autoplay: true,  // Ativar a reprodução automática
-        autoplaySpeed: 3000,  // Intervalo de tempo entre os slides (em milissegundos)
+        autoplaySpeed: 2000,  // Intervalo de tempo entre os slides (em milissegundos)
         dots: true,  // Mostrar indicadores de pontos
         infinite: true,  // Repetir o carrossel indefinidamente
         speed: 500,  // Velocidade de transição (em milissegundos)
