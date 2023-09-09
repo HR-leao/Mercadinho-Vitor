f16e4b73f10833f7de08455a66ad9073f8496458
Hendrio <bigdatahi7@gmail.com>
2023-09-09 17:45:31 -0300

    Up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,40 @@
 
+//CONFIGURAÇÃO DO SLIDE VIA BIBLIOTECA JQUERY USANDO O SLICK
 
+
+
+
+/* 
+LINKS  DEPENDÊNCIAS PARA O FUNCIONAMENTO DO SLICK
+
+    
+	LINK CSS PARA USAR O slick
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
+	
+	
+	LINK SCRIPT PARA IMPORTAR BIBLIOTECA JQUERY	
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+	
+	
+	
+	
+	JAVASCRIPT, A CONFIGURAÇÃO NECESSÁRIA PARA O SLICK FUNCIONAR
+	$(document).ready(function () {
+    $('.slides').slick({
+        autoplay: true,  // Ativar a reprodução automática
+        autoplaySpeed: 3000,  // Intervalo de tempo entre os slides (em milissegundos)
+        dots: true,  // Mostrar indicadores de pontos
+        infinite: true,  // Repetir o carrossel indefinidamente
+        speed: 500,  // Velocidade de transição (em milissegundos)
+        fade: false,  // Ativar transições de fade
+        cssEase: 'linear'  // Tipo de transição
+    });
+});
+
+
+*/
 $(document).ready(function () {
     $('.slides').slick({
         autoplay: true,  // Ativar a reprodução automática
