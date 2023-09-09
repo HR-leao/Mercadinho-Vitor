6e0f3be8dc50d7302842f91b9e3318a8128e6b96
Hendrio <bigdatahi7@gmail.com>
2023-09-09 17:13:25 -0300

    Nova-versão-Slide

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,122 +1,17 @@
-//COLOCANDO COR NO FUNDO DO BOTÃO DO SLIDE
-let manualBtn = document.querySelectorAll('.manual-btn').forEach((e)=>{
-    e.addEventListener('click', ()=>{
-    // Remover a classe 'active' de todos os elementos com a mesma classe
-        document.querySelectorAll('.manual-btn').forEach((ele)=>{
-        ele.classList.remove('active');
-        
-        });
-    // Adicionar a classe 'active' ao elemento clicado
-        e.classList.add('active')
-    })
-     
-})
-//___________________________________________________________________________________________________
+
+
+$(document).ready(function () {
+    $('.slides').slick({
+        autoplay: true,  // Ativar a reprodução automática
+        autoplaySpeed: 3000,  // Intervalo de tempo entre os slides (em milissegundos)
+        dots: true,  // Mostrar indicadores de pontos
+        infinite: true,  // Repetir o carrossel indefinidamente
+        speed: 500,  // Velocidade de transição (em milissegundos)
+        fade: false,  // Ativar transições de fade
+        cssEase: 'linear'  // Tipo de transição
+    });
+});
 
 
 
-/*VARIÁVEL CRIADA PARA IR MUDANDO O VALOR DO MEU ID E AUTOMÁTICAMENTE VAI TROCANDO */
-let count = 1;
-
-/*SELEÇÃO DO  INPUT RADIOS  PARA USAR EM CONJUNTO COM A VARIÁVEL COUNT E IR TROCANDO PARA #raio1, #radio2 #radio3*/
-let radio = document.getElementById('radio1')
-let radio2 = document.getElementById('radio2')
-let radio3 = document.getElementById('radio3')
-
-
-
-/*SELEÇÃO DOS BOTÕES AUTOMÁTICOS DO SLIDE */
-let auto1 = document.querySelector('.auto-btn1')
-let auto2 = document.querySelector('.auto-btn2')
-let auto3 = document.querySelector('.auto-btn3')
-
-
-/*VARIÁVEL tet ESTÁ SERVINDO PARA CRIAR UMA FUNÇÃO DE CLICK QUE IRA REMOVER A CLASS ACTIVE DE TODOS OS BOTÕES DO SLIDE */
-let mnBotao = document.querySelectorAll('.manual-btn')
-
-
-let radios = document.getElementsByClassName('.radios')
-
-
-
-/*AO CLICAR NO BOTÃO MANUAL ELE REMOVE A CLASS ACTIV */
-mnBotao.forEach((e)=>{
-    e.addEventListener('click', ()=>{
-
-        let t = e.getAttribute('data-valor')
-        if(e){
-             auto3.classList.remove('active')
-             auto2.classList.remove('active')
-             auto1.classList.remove('active')
-             setTimeout(function(){
-                e.classList.remove('active')
-             },1000)
-        }
-        if(t === '1') {
-            count = 1;
-        }
-        if(t === '2'){
-            count = 2;
-        }
-        if(t === '3'){
-            count = 3;
-        }
-    })
-
-})
-
-
-
-
-
-/*REMOVE A CLASSE ACTIVE E ADICONA NO RADIUS QUE FOI CLICADO*/
-function poeActive () {
-    
-    
-        setInterval(function(){
-        if(count === 1){
-        auto1.classList.add('active')
-        auto3.classList.remove('active')
-        console.log(count)
-    }
-        },1000)
-    
-    setInterval(function(){
-        if(count === 2){
-        auto1.classList.remove('active')
-        auto2.classList.add('active')
-        console.log(count)
-        
-    }
-    },1000)
-    setInterval(function(){
-        if(count=== 3) {
-        auto2.classList.remove('active')
-        auto3.classList.add('active')
-        console.log(count)
-    }
-    },1000)
-}
-
-poeActive()
-setInterval(function(){
-    poeActive()
-},1000)
-
-setInterval(function() {
-    nextImage()
-    
-}, 5000)
-
-
-function nextImage() {
-    console.log('Entrou no FOR'+count)
-    count++;
-    if(count > 3) {
-        count =1;
-        
-    }
-    document.querySelector('#radio'+count).checked = true;
-
-}
 
