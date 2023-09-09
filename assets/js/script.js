8d423218023a06954dca933ab4e247ab979bb0cb
Hendrio <bigdatahi7@gmail.com>
2023-09-09 18:18:39 -0300

    Bug-Resolvido
    Estava Dando um flash quando alternava entre o último e o primeiro slide

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,18 +35,15 @@ LINKS  DEPENDÊNCIAS PARA O FUNCIONAMENTO DO SLICK
 
 
 */
+
 $(document).ready(function () {
     $('.slides').slick({
         autoplay: true,  // Ativar a reprodução automática
-        autoplaySpeed: 2000,  // Intervalo de tempo entre os slides (em milissegundos)
+        autoplaySpeed: 4000,  // Intervalo de tempo entre os slides (em milissegundos)
         dots: true,  // Mostrar indicadores de pontos
         infinite: true,  // Repetir o carrossel indefinidamente
-        speed: 500,  // Velocidade de transição (em milissegundos)
+        speed: 1000,  // Velocidade de transição (em milissegundos)
         fade: false,  // Ativar transições de fade
         cssEase: 'linear'  // Tipo de transição
     });
 });
-
-
-
-
