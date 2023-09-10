4624d0bd0c43cd5c85a9756aa966dd582ec597cb
Hendrio <bigdatahi7@gmail.com>
2023-09-10 19:43:35 -0300

    Up

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,16 +39,36 @@ LINKS  DEPENDÊNCIAS PARA O FUNCIONAMENTO DO SLICK
 
 $(document).ready(function () {
     $('.slides').slick({
+
+        vertical:true,
         autoplay: true,  // Ativar a reprodução automática
-        autoplaySpeed: 3000,  // Intervalo de tempo entre os slides (em milissegundos)
-        dots: true,  // Mostrar indicadores de pontos
+        autoplaySpeed: 2000,  // Intervalo de tempo entre os slides (em milissegundos)
+        dots: false,  // Mostrar indicadores de pontos
         infinite: true,  // Repetir o carrossel indefinidamente
         speed: 500,  // Velocidade de transição (em milissegundos)
-        fade: true,  // Ativar transições de fade
-        cssEase: 'linear'  // Tipo de transição
+        fade: false,  // Ativar transições de fade
+        cssEase: 'ease-in-out'  // Tipo de transição
     });
 
 });
+
+/*FIM DO SLIDE */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
