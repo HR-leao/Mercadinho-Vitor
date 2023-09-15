61b196b226a05a4cd48b5ca721ed1f71a995182c
Hendrio <bigdatahi7@gmail.com>
2023-09-15 20:25:18 -0300

    up

--- assets/js/produtos.js
+++ assets/js/produtos.js
@@ -5,11 +5,11 @@ let produtosJson = [
     { id: 3, name: ' Coca cola', img: 'assets/images-Produtos/coca-cola-2l.png', price: 10, size: ['2L'], description: 'Refrigerante Coca Cola' },
     { id: 4, name: ' Mineirinho', img: 'assets/images-Produtos/mineirinho2l.png', price: 10, size: ['2L'], description: 'Refrigerante Mineirinho' },
     { id: 5, name: ' Kuat', img: 'assets/images-Produtos/kuat-2l.png', price: 10, size: ['2L'], description: 'Refrigerante Kuat' },
-    { id: 6, name: ' Império', img: '', price: 10, size: [], description: 'Cerveja Império' },
-    { id: 7, name: ' Antártica', img: '', price: 10, size: [], description: 'Cerveja Antártica' },
-    { id: 8, name: ' Brama', img: '', price: 10, size: [], description: 'Cerveja Brama' },
-    { id: 9, name: ' Heineken', img: '', price: 10, size: [], description: 'Cerveja Heineken' },
-    { id: 10, name: ' Itaipava', img: '', price: 10, size: [], description: 'Cerveja Itaipava' },
+    { id: 6, name: ' Império', img: 'assets/images-Produtos/imperio.png', price: 10, size: [], description: 'Cerveja Império' },
+    { id: 7, name: ' Antártica', img: 'assets/images-Produtos/antartica.png', price: 10, size: [], description: 'Cerveja Antártica' },
+    { id: 8, name: ' Brama', img: 'assets/images-Produtos/brama.png', price: 10, size: [], description: 'Cerveja Brama' },
+    { id: 9, name: ' Heineken', img: 'assets/images-Produtos/heineken.png', price: 10, size: [], description: 'Cerveja Heineken' },
+    { id: 10, name: ' Itaipava', img: 'assets/images-Produtos/itaipava.png', price: 10, size: [], description: 'Cerveja Itaipava' },
 
 
 
