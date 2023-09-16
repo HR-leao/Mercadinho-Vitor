f79b78a8328006b22ad531ad0214eb42f5829ef9
Hendrio <bigdatahi7@gmail.com>
2023-09-15 21:14:43 -0300

    up

--- assets/js/produtos.js
+++ assets/js/produtos.js
@@ -4,12 +4,12 @@ let produtosJson = [
     { id: 2, name: ' Grapete', img: 'assets/images-Produtos/grapete-2l.png', price: 10, size: ['2L'], description: 'Refrigerante' },
     { id: 3, name: ' Coca cola', img: 'assets/images-Produtos/coca-cola-2l.png', price: 10, size: ['2L'], description: 'Refrigerante' },
     { id: 4, name: ' Mineirinho', img: 'assets/images-Produtos/mineirinho2l.png', price: 10, size: ['2L'], description: 'Refrigerante' },
-    { id: 5, name: ' Kuat', img: 'assets/images-Produtos/kuat-2l.png', price: 10, size: ['2L'], description: 'Refrigerante Kuat' },
-    { id: 6, name: ' Império', img: 'assets/images-Produtos/imperio.png', price: 10, size: [], description: 'Cerveja Império' },
-    { id: 7, name: ' Antártica', img: 'assets/images-Produtos/antartica.png', price: 10, size: [], description: 'Cerveja Antártica' },
-    { id: 8, name: ' Brama', img: 'assets/images-Produtos/brama.png', price: 10, size: [], description: 'Cerveja Brama' },
-    { id: 9, name: ' Heineken', img: 'assets/images-Produtos/heineken.png', price: 10, size: [], description: 'Cerveja Heineken' },
-    { id: 10, name: ' Itaipava', img: 'assets/images-Produtos/itaipava.png', price: 10, size: [], description: 'Cerveja Itaipava' },
+    { id: 5, name: ' Kuat', img: 'assets/images-Produtos/kuat-2l.png', price: 10, size: ['2L'], description: 'Refrigerante' },
+    { id: 6, name: ' Império', img: 'assets/images-Produtos/imperio.png', price: 10, size: [], description: 'Cerveja' },
+    { id: 7, name: ' Antártica', img: 'assets/images-Produtos/antartica.png', price: 10, size: [], description: 'Cerveja' },
+    { id: 8, name: ' Brama', img: 'assets/images-Produtos/brama.png', price: 10, size: [], description: 'Cerveja' },
+    { id: 9, name: ' Heineken', img: 'assets/images-Produtos/heineken.png', price: 10, size: [], description: 'Cerveja' },
+    { id: 10, name: ' Itaipava', img: 'assets/images-Produtos/itaipava.png', price: 10, size: [], description: 'Cerveja' },
 
 
 
