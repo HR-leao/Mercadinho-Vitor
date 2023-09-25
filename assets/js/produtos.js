e922afc8555593f47c506ec7af7a6a7a3d095edb
Hendrio <bigdatahi7@gmail.com>
2023-09-25 19:03:23 -0300

    up

--- assets/js/produtos.js
+++ assets/js/produtos.js
@@ -1,25 +1,18 @@
 
 let produtosJson = [
-   
+    { id: 1, name: 'Grapete', img: 'assets/images-Produtos/grapete-2l.png', price: 10, size: ['2L'], description: 'Refrigerante' },
+    { id: 2, name: 'Cliper', img: 'assets/images-Produtos/cliper-cola-2l.png', price: 10, size: ['2L'], description: 'Refrigerante Cola' },
+    { id: 3, name: 'Coca cola', img: 'assets/images-Produtos/coca-cola-2l.png', price: 10, size: ['2L'], description: 'Refrigerante' },
+    { id: 4, name: 'Mineirinho', img: 'assets/images-Produtos/mineirinho2l.png', price: 10, size: ['2L'], description: 'Refrigerante' },
+    { id: 5, name: 'Kuat', img: 'assets/images-Produtos/kuat-2l.png', price: 10, size: ['2L'], description: 'Refrigerante Guaraná' },
     { id: 6, name:'Império', img: 'assets/images-Produtos/imperio.png', price: 10, size: [], description: 'Cerveja' },
     { id: 7, name:'Antártica', img: 'assets/images-Produtos/antartica.png', price: 10, size: [], description: 'Cerveja'},
     { id: 8, name:'Brama', img: 'assets/images-Produtos/brama.png', price: 10, size: [], description: 'Cerveja'},
     { id: 9, name:'Heineken', img: 'assets/images-Produtos/heineken.png', price: 10, size: [], description: 'Cerveja'},
     { id: 10, name:'Itaipava', img: 'assets/images-Produtos/itaipava.png', price: 10, size: [], description: 'Cerveja'},
-
+    { id: 11, name: 'Cliper', img: 'assets/images-Produtos/cliper-limao2l.png', price: 10, size: ['2L'], description: 'Refrigerante Limão' },
+    { id: 12, name: 'Cliper', img: 'assets/images-Produtos/cliper-guarana-2l.png', price: 10, size: ['2L'], description: 'Refrigerante Guaraná' }
 
 ]
 
-
-
-let RefriJson = [
-    { id: 1, name: 'Grapete', img: 'assets/images-Produtos/grapete-2l.png', price: 10, size: ['2L'], description: 'Refrigerante'},
-    { id: 2, name: 'Cliper Cola',img: 'assets/images-Produtos/cliper-cola-2l.png', price: 10, size: ['2L'], description: 'Refrigerante'},
-    { id: 3, name: 'Coca cola', img: 'assets/images-Produtos/coca-cola-2l.png', price: 10, size: ['2L'], description: 'Refrigerante'},
-    { id: 4, name: 'Mineirinho', img: 'assets/images-Produtos/mineirinho2l.png', price: 10, size: ['2L'], description: 'Refrigerante'},
-    { id: 5, name: 'Kuat', img: 'assets/images-Produtos/kuat-2l.png', price: 10, size: ['2L'], description: 'Refrigerante' },
-    { id: 6, name: 'Cliper', img: 'assets/images-Produtos/cliper-limao2l.png', price: 10, size: ['2L'], description: 'Limão' },
-    { id: 7, name: 'Cliper', img: 'assets/images-Produtos/cliper-guarana-2l.png', price: 10, size: ['2L'], description: 'Guaraná' }
-    
-]
 
